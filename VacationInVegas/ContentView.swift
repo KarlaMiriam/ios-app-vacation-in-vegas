b7c5c2e21f396075b32050956ccf1c2e11b6393f
Karla <karlamiriam6@gmail.com>
2024-10-03 22:14:27 -0300

    Initial Commit

---
+++ VacationInVegas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VacationInVegas
+//
+//  Created by Karla Miriam Dos santos Goncalves on 03/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VacationInVegasTests/VacationInVegasTests.swift
@@ -0,0 +1,17 @@
+//
+//  VacationInVegasTests.swift
+//  VacationInVegasTests
+//
+//  Created by Karla Miriam Dos santos Goncalves on 03/10/24.
+//
+
+import Testing
+@testable import VacationInVegas
+
+struct VacationInVegasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
